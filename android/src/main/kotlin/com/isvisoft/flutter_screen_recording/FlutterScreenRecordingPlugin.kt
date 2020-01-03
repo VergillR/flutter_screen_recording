6da24959b6725f8071fc410e5c36d4c3e24387f4
Vergill Lemmert <39799314+VergillR@users.noreply.github.com>
2020-01-03 02:20:31 +0100

    Update

--- android/src/main/kotlin/com/isvisoft/flutter_screen_recording/FlutterScreenRecordingPlugin.kt
+++ android/src/main/kotlin/com/isvisoft/flutter_screen_recording/FlutterScreenRecordingPlugin.kt
@@ -71,8 +71,6 @@ class FlutterScreenRecordingPlugin(
                 //It is important to call this before starting the recording
 
                 //Start screen recording
-                hbRecorder?.enableCustomSettings()
-                hbRecorder?.setVideoEncoder("VP8")
                 hbRecorder?.setOutputPath(storePath)
                 hbRecorder?.isAudioEnabled(false)
                 hbRecorder?.recordHDVideo(false)
